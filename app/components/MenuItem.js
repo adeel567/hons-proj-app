d3cbc22677ac1a136a8ad219c877bc8bd7c53fc0
adeelhussain <45519601+adeel567@users.noreply.github.com>
2023-01-13 19:49:49 +0000

    Add view item and fix push.

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -3,20 +3,35 @@ import * as React from 'react';
 import { View } from 'react-native';
 import { FlatList } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph, Subheading } from 'react-native-paper';
+import { BASE_URL } from '../api';
 import { AddToCartButton } from './AddToCartButton';
+import { RemoveFromCartButton } from './RemoveFromCartButton';
+
 
 export const MenuItem = (props) => {
+
+    const hideModal = () => {
+        props.setVisible(false)
+    }
+
     return(
     <Card style={{marginBottom:30, marginHorizontal:25}}>
-        <Card.Cover source={{ uri: props.image}} />
+        <Card.Cover source={{ uri: BASE_URL.concat(props.item.image)}} />
         <Card.Content>
             <Title style={{marginTop:7}}>{props.item.name}</Title>
             <Paragraph>{props.item.description}</Paragraph>
             <Subheading>£{Number(((props.item.pence)/100)).toFixed(2)}</Subheading>
         </Card.Content>
-        <Card.Actions style={{alignSelf: "center"}}>
-            <AddToCartButton color="goldenrod" navigation={props.navigation} id={props.item.id}>Add to cart</AddToCartButton>
+        {(!(props.cart)) ?
+        <Card.Actions style={{alignSelf: "center"}} >
+            <AddToCartButton function={hideModal} color="goldenrod" id={props.item.id} navigation={props.navigation}>Add to cart</AddToCartButton>
         </Card.Actions>
+        :
+        <Card.Actions style={{alignSelf: "center"}}>
+            <AddToCartButton function={hideModal} id={props.item.id} navigation={props.navigation}>Add another</AddToCartButton>
+            <RemoveFromCartButton function={hideModal} id={props.item.id} navigation={props.navigation}>Remove</RemoveFromCartButton>
+        </Card.Actions>
+        }
     </Card>
     )
 }
